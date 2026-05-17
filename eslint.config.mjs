6711697589d3846a2ce43060c45d8dbe8c1aaa8b
Moynul Islam <moynul200215@gmail.com>
2026-05-18 01:51:49 +0600

    chore: set up linter, formatter and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import next from 'eslint-config-next';
+import prettier from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  next,
+
+  // disables conflicting ESLint formatting rules
+  prettier,
+
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'node_modules/**']),
 ]);
 
 export default eslintConfig;
