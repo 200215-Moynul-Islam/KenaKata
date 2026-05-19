2ba4c32119efa4a2749f9d40c2ed870d7ef4d42c
Moynul Islam <moynul200215@gmail.com>
2026-05-19 17:05:40 +0600

    feat: implement basic product page.

--- components/product/product-card.tsx
+++ components/product/product-card.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 type ProductCardProps = {
   product: {
     id: number;
@@ -11,17 +13,22 @@ export default function ProductCard({ product }: ProductCardProps) {
   const imageUrl = product.images?.[0];
 
   return (
-    <div className="group cursor-pointer">
+    <Link href={`/products/${product.id}`} className="group block cursor-pointer">
       <div className="aspect-[3/4] overflow-hidden rounded-xl bg-surface-container">
         <img
           src={imageUrl}
           alt={product.title}
-          className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
+          className="
+            h-full w-full object-cover
+            transition-transform duration-500
+            group-hover:scale-105
+          "
         />
       </div>
 
       <h3 className="mt-2 text-sm font-medium">{product.title}</h3>
+
       <p className="text-primary">${product.price}</p>
-    </div>
+    </Link>
   );
 }
