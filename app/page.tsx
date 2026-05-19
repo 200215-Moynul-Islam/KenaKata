a1b5a2a89bf5f637153bd12346ac2422e62b3258
Moynul Islam <87566898+200215-Moynul-Islam@users.noreply.github.com>
2026-05-19 13:28:34 +0600

    feat: implement the home page carousel (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import HeroCarousel from '@/components/hero-carousel/hero-carousel';
+
 export default function HomePage() {
   return (
     <main>
-      <h1>KenaKata.com</h1>
+      <HeroCarousel />
     </main>
   );
 }
